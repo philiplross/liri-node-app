f1145908f7ef369d385e138664e45e2cc139d23b
Philip Ross <philiplross@gmail.com>
2018-10-20 12:57:49 -0700

    ajax upload

--- liri.js
+++ liri.js
@@ -1,11 +1,109 @@
-require("dotenv").config();
+// require("dotenv").config();
 
 
 var spotifykey = require("keys.js");
 var spotify = new Spotify(keys.spotify);
 
-var concerts
-var movies
-var spotSong
+var concerts = this.spotify.concerts;
+var movies = this.spotify.movies;
+var spotSong = this.spotify.spotSong;
+
+console.log(concerts, movies, spotSong);
+
+
+const app = {};
+
+app.getArists = (artist) => $.ajax({
+  url: 'https://api.spotify.com/v1/search',
+  method: 'GET',
+  dataType: 'json',
+  
+  data: {
+    type: 'artist',
+    q: artist
+  }
+});
+
+app.getAristsAlbums = (id) => $.ajax({
+  url: `https://api.spotify.com/v1/artists/${id}/albums`,
+  method: 'GET',
+  dataType: 'json',
+  data: {
+    album_type: 'album',
+  }
+});
+
+app.getAlbumTracks = (id) => $.ajax({
+  url: `https://api.spotify.com/v1/albums/${id}/tracks`,
+  method: 'GET',
+  dataType: 'json'
+});
+
+app.getAlbums = function (artists) {
+  let albums = artists.map(artist => app.getAristsAlbums(artist.id));
+  $.when(...albums)
+    .then((...albums) => {
+      let albumIds = albums
+        .map(a => a[0].items)
+        .reduce((prev, curr) => [...prev, ...curr], [])
+        .map(album => app.getAlbumTracks(album.id));
+
+      app.getTracks(albumIds);
+    });
+};
+
+app.getTracks = function (tracks) {
+  $.when(...tracks)
+    .then((...tracks) => {
+      tracks = tracks
+        .map(getDataObject)
+        .reduce((prev, curr) => [...prev, ...curr], []);
+      const randomPlayList = getRandomTracks(30, tracks);
+      app.createPlayList(randomPlayList);
+    })
+};
+
+app.createPlayList = function (songs) {
+  const baseUrl = 'https://embed.spotify.com/?theme=white&uri=spotify:trackset:My Playlist:';
+  songs = songs.map(song => song.id).join(',');
+  $('.loader').removeClass('show');
+  $('.playlist').append(`<iframe src="${baseUrl + songs}" height="400"></iframe>`);
+}
+
+app.init = function () {
+  $('form').on('submit', function (e) {
+    e.preventDefault();
+    let artists = $('input[type=search]').val();
+    $('.loader').addClass('show');
+    artists = artists
+      .split(',')
+      .map(app.getArists);
+
+    $.when(...artists)
+      .then((...artists) => {
+        artists = artists.map(a => a[0].artists.items[0]);
+        console.log(artists);
+        app.getAlbums(artists);
+      });
+  });
+
+}
+
+const getDataObject = arr => arr[0].items;
+
+function getRandomTracks(num, tracks) {
+  const randomResults = [];
+  for (let i = 0; i < num; i++) {
+    randomResults.push(tracks[Math.floor(Math.random() * tracks.length)])
+  }
+  return randomResults;
+}
+
+//Uncaught ReferenceError: $ is not defined at script2.js:89
+$(app.init);
+
+
+
+
 
 
